4b21ab9898339c55f7cd5a835230fc55c5aa96db
Semion Prikhodko <semion.ababo@gmail.com>
2012-12-12 01:42:59 +0200

    Kernel loading implemented

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 IMAGE=floppy.img
+KERNEL=kernel.bin
 
 run: image
 	screen qemu-system-x86_64 -fda $(IMAGE) -boot a -no-kvm -curses
 
 image: boot
 	ld -Ttext 0x7C00 --oformat binary -o $(IMAGE) boot.o
+	cat $(KERNEL) >> $(IMAGE)
 
 boot: boot.s
 	as -o boot.o boot.s
--- boot.s
+++ boot.s
@@ -1,6 +1,11 @@
 .set LOAD_ADDRESS, 0x7C00
 .set MAGIC_WORD, 0xAA55
-.set BLOCK_SIZE, 512
+.set SECTOR_SIZE, 512
+.set SECTORS_PER_TRACK, 18
+.set NUMBER_OF_HEADS, 2
+.set DRIVE_NUMBER, 0
+.set KERNEL_ADDRESS, LOAD_ADDRESS + SECTOR_SIZE
+.set KERNEL_LIMIT_ADDRESS, 0x9F600
 
 .section .text
 .code16
@@ -12,20 +17,54 @@ _start: xorw %ax, %ax
         movw %ax, %ss
         movw $LOAD_ADDRESS, %sp
 
+        movw $1, %ax
+        movw $(LOAD_ADDRESS + SECTOR_SIZE), %bx
+        movw $((KERNEL_LIMIT_ADDRESS - KERNEL_ADDRESS) / SECTOR_SIZE), %cx
+        call load
+
         movw $msg, %si
+        call puts
+        movw $(LOAD_ADDRESS + SECTOR_SIZE), %si
+        call puts
+        movw $(LOAD_ADDRESS + 2 * SECTOR_SIZE), %si
         call puts
         hlt
 
+        /* ds:si - string address */
 puts:   lodsb
         cmpb  $0x0, %al
         je puts1
-        movw  $0x7, %bx
-        movb  $0xe, %ah
+        movw  $0x07, %bx
+        movb  $0x0E, %ah
         int   $0x10
         jmp puts
 puts1:  ret
 
+        /* ax - index of first sector
+           es:bx - destination address
+           cx - number of sectors */
+load:   pusha
+        movb $SECTORS_PER_TRACK, %dl
+        divb %dl                        /* T = N / SECTORS_PER_TRACK */
+        movb %ah, %cl
+        incb %cl                        /* S = N mod SECTORS_PER_TRACK + 1 */
+        xorb %ah, %ah
+        movb $NUMBER_OF_HEADS, %dl
+        divb %dl
+        movb %al, %ch                   /* C = T / NUMBER_OF_HEADS */
+        movb %ah, %dh                   /* H = T mod NUMBER_OF_HEADS */
+        movb $DRIVE_NUMBER, %dl
+        movw $0x0201, %ax
+        int $0x13
+        movw %es, %ax
+        addw $(SECTOR_SIZE >> 4), %ax
+        movw %ax, %es
+        popa
+        incw %ax
+        loop load
+        ret
+
 msg:    .asciz "Hello World!\r\n"
 
-        .org BLOCK_SIZE - 2
+        .org SECTOR_SIZE - 2
         .word MAGIC_WORD
